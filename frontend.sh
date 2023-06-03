bc20d3ec36388706a39c075ea1b14740d15006a2
Vajram Gajengi <vajramg@gmail.com>
2023-06-03 19:56:09 +0530

    frontend shell commands

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,11 @@
 yum install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+systemctl restart nginx
+systemctl enable nginx
 
 #create config file
